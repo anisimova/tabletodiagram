38ddfe4e48875e78f2341487b78a4f21e90ebb9b
anisimova <nadya.anisimova@gmail.com>
2015-05-27 16:23:13 +0300

    5

--- popup.js
+++ popup.js
@@ -7,8 +7,7 @@ function create(valueTab) {
     $.each(valueTab, function(){
         $('#tabledata').append('<p>' + this + '</p>');
     });
-    //
-    /*var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
+//линии - не работает?!
     var lineChartData = {
             labels : ["January","February","March","April","May","June","July"],
             datasets : [
@@ -17,71 +16,109 @@ function create(valueTab) {
                     fillColor : "rgba(220,220,220,0.2)",
                     strokeColor : "rgba(220,220,220,1)",
                     pointColor : "rgba(220,220,220,1)",
-                    pointStrokeColor : "#fff",
-                    pointHighlightFill : "#fff",
+                    pointStrokeColor : "#f00",
+                    pointHighlightFill : "#a00",
                     pointHighlightStroke : "rgba(220,220,220,1)",
-                    data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+                    data : [30,50,22,10,90,35,0]
                 },
                 {
                     label: "My Second dataset",
                     fillColor : "rgba(151,187,205,0.2)",
                     strokeColor : "rgba(151,187,205,1)",
                     pointColor : "rgba(151,187,205,1)",
-                    pointStrokeColor : "#fff",
-                    pointHighlightFill : "#fff",
+                    pointStrokeColor : "#0f0",
+                    pointHighlightFill : "#0a0",
                     pointHighlightStroke : "rgba(151,187,205,1)",
-                    data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+                    data : [60,90,10,25,15,75,0]
                 }
             ]
 
-    }
+    };
     
-        var ctx = document.getElementById("chart").getContext("2d");
-        window.myLine = new Chart(ctx).Line(lineChartData, {
-            responsive: true
-        });
-    var c = document.getElementById("chart");
-var ctx = c.getContext("2d");
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(0,0,150,75);
-*/
-var pieData = [
-                {
-                    value: 300,
-                    color:"#F7464A",
-                    highlight: "#FF5A5E",
-                    label: "Red"
-                },
-                {
-                    value: 50,
-                    color: "#46BFBD",
-                    highlight: "#5AD3D1",
-                    label: "Green"
-                },
-                {
-                    value: 100,
-                    color: "#FDB45C",
-                    highlight: "#FFC870",
-                    label: "Yellow"
-                },
-                {
-                    value: 40,
-                    color: "#949FB1",
-                    highlight: "#A8B3C5",
-                    label: "Grey"
-                },
-                {
-                    value: 120,
-                    color: "#4D5360",
-                    highlight: "#616774",
-                    label: "Dark Grey"
-                }
+    var ctx = document.getElementById("chart").getContext("2d");
+    window.myLine = new Chart(ctx).Line(lineChartData, {responsive: true});
+    
+//просто канвас - работает
+    /*var c = document.getElementById("chart");
+    var ctx = c.getContext("2d");
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(0,0,150,75);*/
+//pie-работает
+    /*var pieData = [
+                    {
+                        value: 300,
+                        color:"#F7464A",
+                        highlight: "#FF5A5E",
+                        label: "Red"
+                    },
+                    {
+                        value: 50,
+                        color: "#46BFBD",
+                        highlight: "#5AD3D1",
+                        label: "Green"
+                    },
+                    {
+                        value: 100,
+                        color: "#FDB45C",
+                        highlight: "#FFC870",
+                        label: "Yellow"
+                    },
+                    {
+                        value: 40,
+                        color: "#949FB1",
+                        highlight: "#A8B3C5",
+                        label: "Grey"
+                    },
+                    {
+                        value: 120,
+                        color: "#4D5360",
+                        highlight: "#616774",
+                        label: "Dark Grey"
+                    }
 
-            ];
+    ];
+    var ctx = document.getElementById("chart").getContext("2d");
+    window.myPie = new Chart(ctx).Pie(pieData);
+    */
+//бублик - работает
+    /*var doughnutData = [
+                    {
+                        value: 300,
+                        color:"#F7464A",
+                        highlight: "#FF5A5E",
+                        label: "Red"
+                    },
+                    {
+                        value: 50,
+                        color: "#46BFBD",
+                        highlight: "#5AD3D1",
+                        label: "Green"
+                    },
+                    {
+                        value: 100,
+                        color: "#FDB45C",
+                        highlight: "#FFC870",
+                        label: "Yellow"
+                    },
+                    {
+                        value: 40,
+                        color: "#949FB1",
+                        highlight: "#A8B3C5",
+                        label: "Grey"
+                    },
+                    {
+                        value: 120,
+                        color: "#4D5360",
+                        highlight: "#616774",
+                        label: "Dark Grey"
+                    }
 
-                var ctx = document.getElementById("chart").getContext("2d");
-                window.myPie = new Chart(ctx).Pie(pieData);
-           
+    ];
+    var ctx = document.getElementById("chart").getContext("2d");
+    window.myDoughnut = new Chart(ctx).Doughnut(doughnutData, {responsive : true});
+    */
+
+
 }
 
 /* Once the DOM is ready... */
